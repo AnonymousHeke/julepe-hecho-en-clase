ea81e929d38bb95dba7297a9294d525390c84063
Miguel Bayon <miguel.bayon@iessanandres.com>
2017-02-22 18:30:05 +0100

    Ahora los bots tiran la carta conforme a las reglas del tute

--- Jugador.java
+++ Jugador.java
@@ -12,7 +12,6 @@ public class Jugador
     private Carta[] cartasQueTieneEnLaMano;
     private int numeroCartasEnLaMano;
     private ArrayList<Baza> bazasGanadas;
-    
 
     /**
      * Constructor for objects of class Jugador
@@ -25,7 +24,6 @@ public class Jugador
         bazasGanadas = new ArrayList<Baza>();
     }
 
- 
     /**
      * Metodo que hace que el jugador reciba una carta
      */
@@ -36,7 +34,7 @@ public class Jugador
             numeroCartasEnLaMano++;
         }
     }
-    
+
     /**
      * Metodo que muestra las cartas del jugador por pantalla
      */
@@ -49,7 +47,6 @@ public class Jugador
         }
     }
 
-    
     /**
      * Metodo que devuelve el nombre del jugador
      */
@@ -57,7 +54,7 @@ public class Jugador
     {
         return nombre;
     }
-    
+
     /**
      * Metodo que devuelve la carta especificada como parametro si
      * el jugador dispone de ella y simula que se lanza a la mesa
@@ -65,9 +62,9 @@ public class Jugador
     public Carta tirarCarta(String nombreCarta)
     {
         Carta cartaTirada = null;
-        
+
         if (numeroCartasEnLaMano > 0) {
-            
+
             int cartaActual = 0;
             boolean buscando = true;
             while (cartaActual < cartasQueTieneEnLaMano.length && buscando) {
@@ -77,61 +74,87 @@ public class Jugador
                         cartaTirada = cartasQueTieneEnLaMano[cartaActual];
                         cartasQueTieneEnLaMano[cartaActual] = null;
                         numeroCartasEnLaMano--;
-                        System.out.println(nombre + " ha tirado " + cartaTirada);
+                        System.out.print(nombre + " ha tirado " + cartaTirada);
+                        String cartasQueLeQuedan = "";
+                        for (Carta carta : cartasQueTieneEnLaMano) {
+                            if (carta != null) {
+                                cartasQueLeQuedan += carta + " - ";
+                            }
+                        }
+                        
+                        if (cartasQueLeQuedan.length() > 0) {
+                            System.out.println(" (le quedan: " + cartasQueLeQuedan.substring(0, cartasQueLeQuedan.length() - 3) + ")");
+                        }
+                        else {
+                            System.out.println();
+                        }
                     }
                 }
                 cartaActual++;
             }
-            
-            
+
             
         }
-                
+
         return cartaTirada;
     }
-    
-    
+
     
     /**
-     * Método que tira una carta aleatoria 
+     * Método que elige una carta aleatoria 
      */
-    public Carta tirarCartaAleatoria() 
+    public Carta elegirCartaAleatoria() 
     {
         Carta cartaTirada = null;
-        
-        
+
         if (numeroCartasEnLaMano > 0) {
             Random aleatorio = new Random();
-            
+
             boolean elJugadorHaTiradoUnaCarta = false;
             while (elJugadorHaTiradoUnaCarta == false) {
                 int posicionAleatoria = aleatorio.nextInt(5);
                 if (cartasQueTieneEnLaMano[posicionAleatoria] != null) {
                     cartaTirada = cartasQueTieneEnLaMano[posicionAleatoria];
-                    cartasQueTieneEnLaMano[posicionAleatoria] = null;
-                    numeroCartasEnLaMano--;
-                    System.out.println(nombre + " ha tirado " + cartaTirada);
                     elJugadorHaTiradoUnaCarta = true;
                 }
             }
-            
+
         }
-        
+
         return cartaTirada;
     }
-    
-    
+
     /**
      * Método que tira una carta "inteligentemente"
      */
     public Carta tirarCartaInteligentemente(Palo paloPrimeraCartaDeLaBaza, 
                                             Carta cartaQueVaGanando,
                                             Palo paloQuePinta)
-    {
-        return tirarCartaAleatoria();        
+    {        
+        Carta cartaATirar = null;
+
+        // Puede ganar asistiendo?
+        cartaATirar = puedeAsistirGanando(paloPrimeraCartaDeLaBaza, cartaQueVaGanando);
+        if (cartaATirar == null) {        
+            // Puede asistir aunque no gane?
+            cartaATirar = puedeAsistirSinGanar(paloPrimeraCartaDeLaBaza);
+            if (cartaATirar == null) {
+                // Puede ganar fallando?
+                cartaATirar = puedeGanarFallando(cartaQueVaGanando, paloQuePinta);
+                if (cartaATirar == null) {                    
+                    // Toca contrafallar (tirar una aleatoria)  
+                    cartaATirar = elegirCartaAleatoria();   
+                }
+            }
+            
+         
+        }
+
+        tirarCarta(cartaATirar.toString());
+        
+        return cartaATirar;
     }
-    
-    
+
     /**
      * Metodo que hace que jugador recoja una baza ganada
      */
@@ -139,8 +162,7 @@ public class Jugador
     {
         bazasGanadas.add(bazaGanada);
     }
-    
-    
+
     /**
      * Metodo que devuelve el numero de bazas ganadas por el jugador hasta
      * el momento
@@ -149,24 +171,80 @@ public class Jugador
     {
         return bazasGanadas.size();
     }
+
+    
+    /**
+     * Comprueba si un jugador puede ganar la baza asistiendo. Si es asi devuelve la
+     * primer carta con la que es capaz de ganar la baza; si no puede, devuelve null.
+     *
+     * @param paloPrimeraCartaDeLaBaza El palo de inicio de la baza
+     * @param cartaQueVaGanando La carta que va ganando hasta ese momento
+     * @return La primera carta de las que tiene en la mano que gana asistiendo
+     */
+    public Carta puedeAsistirGanando(Palo paloPrimeraCartaDeLaBaza, Carta cartaQueVaGanando) 
+    {
+        Carta cartaADevolver = null;
+        
+        if (paloPrimeraCartaDeLaBaza == cartaQueVaGanando.getPalo()) {
+            int i = 0;
+            while (cartaADevolver == null && i < cartasQueTieneEnLaMano.length) {
+                if (cartasQueTieneEnLaMano[i] != null) {
+                    if ((cartasQueTieneEnLaMano[i].getPalo() == paloPrimeraCartaDeLaBaza) && 
+                        (cartasQueTieneEnLaMano[i].getPosicionEscalaTute() > cartaQueVaGanando.getPosicionEscalaTute())) {
+                            cartaADevolver = cartasQueTieneEnLaMano[i];
+                        }
+                }
+                i++;
+            }            
+        }
+                
+        return cartaADevolver;
+    }
+    
+
+    /**
+     * Comprueba si un jugador tiene una carta al menos con la que asistir. Si la tiene,
+     * devuelve la primera que encuentra; si no la tiene, devuelve null.
+     *
+     * @param paloPrimeraCartaDeLaBaza El palo con el que se inicio la baza
+     * @return La primera carta encontrada que sirve para asistir
+     */
+    public Carta puedeAsistirSinGanar(Palo paloPrimeraCartaDeLaBaza) 
+    {
+        Carta cartaADevolver = null;
+        
+        int i = 0;
+        while (cartaADevolver == null && i < cartasQueTieneEnLaMano.length) {
+            if (cartasQueTieneEnLaMano[i] != null) {
+                if ((cartasQueTieneEnLaMano[i].getPalo() == paloPrimeraCartaDeLaBaza)) {
+                    cartaADevolver = cartasQueTieneEnLaMano[i];                 
+                }
+            }
+            i++;             
+        }
+                
+        return cartaADevolver;
+    }
     
     
+    public Carta puedeGanarFallando(Carta cartaQueVaGanando, Palo paloQuePinta) 
+    {
+        Carta cartaADevolver = null;
+        
+        int i = 0;
+        while (cartaADevolver == null && i < cartasQueTieneEnLaMano.length) {
+            if (cartasQueTieneEnLaMano[i] != null) {
+                if ((cartasQueTieneEnLaMano[i].ganaA(cartaQueVaGanando, paloQuePinta))) {
+                        cartaADevolver = cartasQueTieneEnLaMano[i];                 
+                }
+            }
+            i++;             
+        }
+                
+        return cartaADevolver;
+    }    
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
